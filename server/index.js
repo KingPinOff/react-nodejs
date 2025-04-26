8ae9ebd591e75ecdf268956a53fecb0ad5ebdbec
KingPinOff <nongdambat@gmail.com>
2025-04-26 11:17:31 +0530

    fix the mobile button and updated port too

--- server/index.js
+++ server/index.js
@@ -96,5 +96,5 @@ io.on("connection",(socket)=>{
     
 })
 
-
-server.listen(3000, () => console.log(`Server running on port `));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port${PORT} `));
