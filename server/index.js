33a414242b08476002de97632dedc65076e1a2f6
KingPinOff <nongdambat@gmail.com>
2025-04-26 09:13:49 +0530

    added another port

--- server/index.js
+++ server/index.js
@@ -97,7 +97,5 @@ io.on("connection",(socket)=>{
 })
 
 
-server.listen(3000,()=>{
-    console.log("Server running on 3000"
-    )
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
